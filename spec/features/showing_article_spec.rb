ecaf10793a1c6809697afdf51ae64f519b4ce1e3
Kevin <kevin11068@gmail.com>
2015-12-12 20:19:54 +0800

    Restrict new edit and delete options for signed and owned user

--- spec/features/showing_article_spec.rb
+++ spec/features/showing_article_spec.rb
@@ -3,7 +3,44 @@ require "rails_helper"
 RSpec.feature "showing an Article" do
 	
 	before do 
-		@article = Article.create(title: "The first article", body: "body of first article")
+		@kkk = User.create!(email: "kkk@kkk.com", password: "kkkkkkkk")
+		@ppp = User.create!(email: "ppp@ppp.com", password: "pppppppp")
+
+		@article = Article.create(title: "The first article", body: "body of first article", user: @kkk)
+	end
+
+	scenario "A non-signed in user doesn't see edit and delete" do
+		visit "/"
+		click_link @article.title
+		expect(page).to have_content(@article.title)
+		expect(page).to have_content(@article.body)
+		expect(current_path).to eq(article_path(@article))
+
+		expect(page).not_to have_link("Edit Article")
+		expect(page).not_to have_link("Delete Article")
+
+	end
+
+	scenario "A non-owner signed-in user can't see edit and delete button" do
+		login_as(@ppp)
+		visit "/"
+		click_link @article.title
+
+
+		expect(page).not_to have_link("Edit Article")
+		expect(page).not_to have_link("Delete Article")
+
+	end
+
+	scenario "A signed-in user can see edit and delete button" do
+		login_as(@kkk)
+		visit "/"
+		click_link @article.title
+
+
+		expect(page).to have_link("Edit Article")
+		expect(page).to have_link("Delete Article")
+
 	end
 
 	scenario "Display individual article" do
